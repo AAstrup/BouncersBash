9590bdaa9b71838e12d591571dc5afe62783b70b
AAstrup <alexander.westi.astrup@gmail.com>
2017-09-05 14:22:01 +0200

    Resolve Test

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,8 +5,9 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Test");
-	}
+        //Hi Julian you lying bastard
+        Debug.Log("HELOL");
+    }
 	
 	// Update is called once per frame
 	void Update () {
