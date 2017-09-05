3ff479ed7a2613ed55ed6f4f2e2f807e19930371
julian-abela <abela_julian@yahoo.com>
2017-09-05 13:05:26 +0200

    Test commit

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        Debug.Log("Test");
 	}
 	
 	// Update is called once per frame
