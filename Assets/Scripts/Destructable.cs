2de688835418d61f17ca589b97682d3a3810fa84
AAstrup <alexander.westi.astrup@gmail.com>
2017-09-10 02:50:39 +0200

    Now you can queue destructions

--- Assets/Scripts/Destructable.cs
+++ Assets/Scripts/Destructable.cs
@@ -7,18 +7,19 @@ public class Destructable : MonoBehaviour {
 
     float mapStartTime;
     public bool randomlyDestroyFloor;
-    List<GameObject> gmjToFalling;
+    List<GameObject> gmjFalling;
     GameObject[] gmjToFall;
     List<GameObject> gmjStillAlive;
     int amountGmjFound;
     public float fallSpeed = 3f;
-    public float mapTimeInSeconds = 60f; 
+    public float mapTimeInSeconds = 60f;
+    int? gmjNumberQueuedDestroyTarget = null;
 
     // Use this for initialization
     void Start () {
         mapStartTime = Time.time;
         gmjToFall = new GameObject[10];
-        gmjToFalling = new List<GameObject>();
+        gmjFalling = new List<GameObject>();
         gmjStillAlive = new List<GameObject>();
         for (int i = 0; i <= 9; i++)
         {
@@ -40,14 +41,21 @@ public class Destructable : MonoBehaviour {
         {
             if (Input.GetKeyDown(i.ToString()) && gmjToFall[i] != null)
             {
-                Destroy(i);
+                //Destroy(i); //Destroys immediately
+                SetDestroyTarget(i); //Sets the next block to be destroyed
             }
         }
-        foreach (var item in gmjToFalling)
+        foreach (var item in gmjFalling)
         {
             item.transform.position = new Vector2(item.transform.position.x, item.transform.position.y - fallSpeed * Time.deltaTime);
         }
 	}
+
+    private void SetDestroyTarget(int i)
+    {
+        if (!gmjFalling.Contains(gmjToFall[i]))
+            gmjNumberQueuedDestroyTarget = i;
+    }
 
     private void CheckForRandomGmjToDestroy()
     {
@@ -56,16 +64,28 @@ public class Destructable : MonoBehaviour {
         float timePrFall = mapTimeInSeconds / amountGmjFound;
         if ((Time.time - mapStartTime) > (timePrFall * (amountGmjFound - gmjStillAlive.Count + 1)))//+1 such that a piece is not falling from the start
         {
-            int index = UnityEngine.Random.Range(0, gmjStillAlive.Count);
-            GameObject chosenGmjToDestroy = gmjStillAlive[index];
-            gmjStillAlive.RemoveAt(index);
-            gmjToFalling.Add(chosenGmjToDestroy);
+            GameObject toDestroy;
+            int index;
+            if (gmjNumberQueuedDestroyTarget.HasValue) //Queued gmj will be destroyed
+            {
+                index = gmjNumberQueuedDestroyTarget.Value;
+                toDestroy = gmjToFall[gmjNumberQueuedDestroyTarget.Value];
+                gmjStillAlive.Remove(gmjToFall[index]);
+                gmjNumberQueuedDestroyTarget = null;
+            }
+            else //Random gmj will be destroyed
+            {
+                index = UnityEngine.Random.Range(0, gmjStillAlive.Count);
+                toDestroy = gmjStillAlive[index];
+                gmjStillAlive.RemoveAt(index);
+            }
+            gmjFalling.Add(toDestroy);
         }
     }
 
     private void Destroy(int i)
     {
-        if(!gmjToFalling.Contains(gmjToFall[i]))
-            gmjToFalling.Add(gmjToFall[i]);
+        if(!gmjFalling.Contains(gmjToFall[i]))
+            gmjFalling.Add(gmjToFall[i]);
     }
 }
